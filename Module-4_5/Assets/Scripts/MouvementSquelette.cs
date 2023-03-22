5446f7797b17c60a1de093a92af335bd44f3b78c
Eliefrh <2067396@bdeb.qc.ca>
2023-03-22 08:39:45 -0400

    module 5

---
+++ Module-4_5/Assets/Scripts/MouvementSquelette.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouvementSquelette : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] _pointsPatrouille;
+    private NavMeshAgent _agent;
+    private int _indexPatrouille;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
